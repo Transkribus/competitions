8875c917527931e51d2fad3e241f867152d8b803
Giorgos Sfikas (laptop/ubuntu15) <georges.sfikas@gmail.com>
2016-06-25 14:41:35 +0300

    Register works ok. Added two dummy users to check, one w/ a new affiliation

--- scriptnet/competitions/views.py
+++ scriptnet/competitions/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import get_object_or_404, HttpResponse, render
 from django.template import loader
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 from .forms import LoginForm, RegisterForm
-from .models import Competition, Track, Subtrack
+from .models import Affiliation, Individual, Competition, Track, Subtrack
 
 def index(request):
     login_form = LoginForm()
@@ -17,14 +18,39 @@ def index(request):
         if 'register' in request.POST:
             register_form = RegisterForm(request.POST)
             if register_form.is_valid():
-                #TODO: Actually create the user
-                # user = User.objects.create_user(form.cleaned_data['username'],password=form.cleaned_data['password'],email=form.cleaned_data['email'],first_name=form.cleaned_data['given_name'],last_name=form.cleaned_data['family_name'])
-                #TODO print a message saying that the user is created -- eventually will have to authenticate him by email
+                user = User.objects.create_user(
+                    username=register_form.cleaned_data['username'],
+                    password=register_form.cleaned_data['password'],
+                    email=register_form.cleaned_data['email'],
+                    first_name=register_form.cleaned_data['first_name'],
+                    last_name=register_form.cleaned_data['last_name'])
+
+                affiliations_id = int(register_form.cleaned_data['affiliations'])
+                affiliations_newstring = register_form.cleaned_data['new_affiliation']
+                if(affiliations_id == -1):
+                    #If the user selected 'Other'
+                    if not affiliations_newstring:
+                        #TODO: Redirect to some error page: New affiliation is unspecified
+                        return HttpResponseRedirect('/competitions/')
+                    affiliation = Affiliation.objects.create(name=affiliations_newstring)
+                else:
+                    if affiliations_newstring:
+                        #TODO: Redirect to some error page: User specified a new affilation,
+                        #      but also chose one from the list
+                        return HttpResponseRedirect('/competitions/')                        
+                    affiliation = Affiliation.objects.get(pk=affiliations_id)
+
+                user.individual.shortbio = register_form.cleaned_data['shortbio']
+                user.individual.affiliations.add(affiliation)
+                #TODO: print a message saying that the user is created -- 
+                # eventually will have to authenticate him by email
                 return HttpResponseRedirect('/competitions/')
         elif 'login' in request.POST:
             login_form = LoginForm(request.POST)
             if login_form.is_valid():
-                user = authenticate(username=login_form.cleaned_data['username'], password=login_form.cleaned_data['password'])
+                user = authenticate(
+                    username=login_form.cleaned_data['username'], 
+                    password=login_form.cleaned_data['password'])
                 if user is not None:
                     if user.is_active:
                         login(request, user)
