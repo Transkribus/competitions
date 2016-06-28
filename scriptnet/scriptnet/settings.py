111046a9f073668cc3b0a57d568dc1d56f295997
Giorgos Sfikas (laptop/ubuntu15) <georges.sfikas@gmail.com>
2016-06-28 12:51:16 +0300

    Adds all user,non-static data to a media folder, following django good practices

--- scriptnet/scriptnet/settings.py
+++ scriptnet/scriptnet/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',                
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media -- Uploaded files from users. This may or may not include public & private training & test sets
+
+MEDIA_URL = '/media/'
