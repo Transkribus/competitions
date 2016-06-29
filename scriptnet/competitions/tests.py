29c176914416d0e1dfbd41ba98475adf1824ad24
Giorgos Sfikas <georges.sfikas@gmail.com>
2016-06-29 16:01:42 +0300

    Added a few reminders for tests

--- scriptnet/competitions/tests.py
+++ scriptnet/competitions/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#Add a test to ...
+#TODO: check that all available benchmarks run, and that they give the results that is expected for a preset dataset.
+
+#TODO: login a user and log him out
+
+#TODO: register a new user and delete him
