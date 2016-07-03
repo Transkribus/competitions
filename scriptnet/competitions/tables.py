94d125c985ccfac2e716c42d80f54a7c0029d2ca
Giorgos Sfikas (laptop/ubuntu15) <georges.sfikas@gmail.com>
2016-07-04 00:03:22 +0300

    Trying to show results with django_tables2

--- scriptnet/competitions/tables.py
+++ scriptnet/competitions/tables.py
@@ -4,4 +4,17 @@ from .models import Submission
 class SubmissionTable(tables.Table):
     class Meta:
         model = Submission
+        fields = {'name', 'method_info', 'submitter'}
         attrs = {'class': 'paleblue'}
+        
+    SubmissionStatus_set = tables.Column()        
+    submitter = tables.Column()
+    def render_submitter(self, value):
+        if value is not None:
+            return ', '.join(['{} {}'.format(s.user.first_name, s.user.last_name) for s in value.all()])
+        return '-'
+
+    def render_SubmissionStatus_set(self, value):
+        if value is not None:
+            return ', '.join(['{} {}'.format(s.benchmark.name, s.numericalresult) for s in value.all()])
+        return '-'        
