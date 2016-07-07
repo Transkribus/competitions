959c82c366b35adb9faff59b11dc960116ffa18d
Giorgos Sfikas <georges.sfikas@gmail.com>
2016-07-07 14:01:58 +0300

    Adds nonmatching groups on urls.py; reverse should work now

--- scriptnet/competitions/urls.py
+++ scriptnet/competitions/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^logout$', views.signout, name='signout'),
     url(r'^(?P<competition_id>[0-9]+)/scoreboard$', views.scoreboard, name='scoreboard'),    
-    url(r'^((?P<competition_id>[0-9]+)/)?((?P<track_id>[0-9]+)/)?((?P<subtrack_id>[0-9]+)/)?$', views.competition, name='competition'),
+    url(r'^(?:(?P<competition_id>[0-9]+)/)?(?:(?P<track_id>[0-9]+)/)?(?:(?P<subtrack_id>[0-9]+)/)?$', views.competition, name='competition'),
     url(r'^(?P<competition_id>[0-9]+)/(?P<track_id>[0-9]+)/(?P<subtrack_id>[0-9]+)/submit$', views.submit, name='submit'),
-    url(r'^(?P<competition_id>[0-9]+)/((?P<track_id>[0-9]+)/)?((?P<subtrack_id>[0-9]+)/)?viewresults$', views.viewresults, name='viewresults'),
+    url(r'^(?P<competition_id>[0-9]+)/(?:(?P<track_id>[0-9]+)/)?(?:(?P<subtrack_id>[0-9]+)/)?viewresults$', views.viewresults, name='viewresults'),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url('^', include('django.contrib.auth.urls')),
 ]
