befd40ff2c60aefd1dfbc27a984a9aa4472528d7
Giorgos Sfikas (laptop/ubuntu15) <georges.sfikas@gmail.com>
2016-07-09 12:15:40 +0300

    Adds a sketch of some test cases to be handled

--- scriptnet/competitions/tests.py
+++ scriptnet/competitions/tests.py
@@ -1,10 +1,38 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ModelTests(TestCase):
+    def test_affiliation_prwto(self):
+        self.fail("This test is under construction.")
 
-#Add a test to ...
-#TODO: check that all available benchmarks run, and that they give the results that is expected for a preset dataset.
+    def test_affiliation_deutero(self):
+        self.fail("This test is under construction.")
 
-#TODO: login a user and log him out
+class EvaluatorTests(TestCase):
+    #TODO: check that all available benchmarks run, and that they give the results that is expected for a preset dataset.
+    def test_evaluator_prwto(self):
+        self.assertEqual(1, 1)
 
-#TODO: register a new user and delete him
+class ViewTests(TestCase):
+    def test_index(self):
+        self.fail("This test is under construction")
+
+class AuthenticationTests(TestCase):
+    def test_register(self):
+        #TODO: register a new user and delete him    
+        self.fail("This test is under construction")
+    def test_login_valid_user(self):
+        #Login with an existing user
+        self.assertEqual(1, 1)
+    def test_login_unknown_user(self):
+        #Login with an invalid user
+        self.assertEqual(1, 1)        
+    def test_register_and_login(self):
+        self.fail("This test is under construction")
+
+class FormTests(TestCase):
+    def test_registerform(self):
+        self.fail("")
+    def test_loginform(self):
+        self.fail("")
+    def test_submitresultform(self):
+        self.fail("")
