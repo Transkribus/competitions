e89d3c32401a19c5300dd9b1c3a3fb9b4dd41e9f
Giorgos Sfikas (laptop/ubuntu15) <georges.sfikas@gmail.com>
2016-07-10 13:53:27 +0300

    Fix on urls.py, following results from the recent tests

--- scriptnet/competitions/urls.py
+++ scriptnet/competitions/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^logout$', views.signout, name='signout'),
-    url(r'^(?P<competition_id>[0-9]+)/scoreboard$', views.scoreboard, name='scoreboard'),    
-    url(r'^(?:(?P<competition_id>[0-9]+)/)?(?:(?P<track_id>[0-9]+)/)?(?:(?P<subtrack_id>[0-9]+)/)?$', views.competition, name='competition'),
-    url(r'^(?P<competition_id>[0-9]+)/(?P<track_id>[0-9]+)/(?P<subtrack_id>[0-9]+)/submit$', views.submit, name='submit'),
-    url(r'^(?P<competition_id>[0-9]+)/(?:(?P<track_id>[0-9]+)/)?(?:(?P<subtrack_id>[0-9]+)/)?viewresults$', views.viewresults, name='viewresults'),
+    url(r'^logout/$', views.signout, name='signout'),
+    url(r'^(?P<competition_id>[0-9]+)/scoreboard/$', views.scoreboard, name='scoreboard'),
+    url(r'^(?:(?P<competition_id>[0-9]+)/?)(?:/(?P<track_id>[0-9]+))?(?:/(?P<subtrack_id>[0-9]+))?/$', views.competition, name='competition'),
+    url(r'^(?P<competition_id>[0-9]+)/(?P<track_id>[0-9]+)/(?P<subtrack_id>[0-9]+)/submit/$', views.submit, name='submit'),
+    url(r'^(?P<competition_id>[0-9]+)/(?:(?P<track_id>[0-9]+)/)?(?:(?P<subtrack_id>[0-9]+)/)?viewresults/$', views.viewresults, name='viewresults'),
+    url(r'^$', views.index, name='index'),    
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url('^', include('django.contrib.auth.urls')),
 ]
