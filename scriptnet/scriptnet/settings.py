000b37c5d819bdf809ddc8b2f3f928f3e9e6c3b8
Giorgos Sfikas <georges.sfikas@gmail.com>
2016-07-12 18:38:20 +0300

    Adds STATIC_ROOT on settings and gitignore

--- scriptnet/scriptnet/settings.py
+++ scriptnet/scriptnet/settings.py
@@ -24,7 +24,6 @@ MESSAGE_TAGS = {
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -159,14 +158,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media -- Uploaded files from users. This may or may not include public & private training & test sets
 
 MEDIA_URL = '/media/'
-
 
