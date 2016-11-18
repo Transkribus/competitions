36f26d8421d112e46cb754e2bf3a550021a96b47
Giorgos Sfikas <georges.sfikas@gmail.com>
2016-11-18 17:25:01 +0200

    Checking if environment is loaded correctly With startup code added on apps.py

--- scriptnet/competitions/apps.py
+++ scriptnet/competitions/apps.py
@@ -1,7 +1,20 @@
 from __future__ import unicode_literals
-
 from django.apps import AppConfig
+import os
 
 
 class CompetitionsConfig(AppConfig):
     name = 'competitions'
+    def ready(self):
+        # startup code here        
+        print("""
+##############################################################
+#                                                            #
+#           ScriptNet Competitions - booting!                #
+#                                                            #
+##############################################################        
+# Environment:                                               #
+# SYNTHIMA={}
+#
+        """.format(os.environ.get('SYNTHIMA'))
+        )
