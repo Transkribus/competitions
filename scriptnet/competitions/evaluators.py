76e551ed97022a6c2eb9987426e590ed1d75d559
Giorgos Sfikas <georges.sfikas@gmail.com>
2017-02-02 17:08:44 +0200

    CVL - ICDAR Writer identification 2017 (#78)
    * Writer Identification Competition initial commit
    
    * pep8 coding style
    
    * small fix
    
    * pep8 ... this time for real
    
    * again pep :(

--- scriptnet/competitions/evaluators.py
+++ scriptnet/competitions/evaluators.py
@@ -1,6 +1,6 @@
 # This file contains all functions available to compute numerical results
 # for user uploaded method results.
-# Except: evaluator_worker . This is used as the thread that wraps calls 
+# Except: evaluator_worker . This is used as the thread that wraps calls
 # to evaluator_function in views.py .
 
 from django.conf import settings
@@ -17,9 +17,10 @@ import tarfile
 
 temporary_folder = '/tmp/'
 
+
 def cmdline(command, *args, **kwargs):
-    # http://stackoverflow.com/questions/3503879/assign-output-of-os-system-to-a-variable-and-prevent-it-from-being-displayed-on    
-    # http://stackoverflow.com/questions/17615414/how-to-convert-binary-string-to-normal-string-in-python3 
+    # http://stackoverflow.com/questions/3503879/assign-output-of-os-system-to-a-variable-and-prevent-it-from-being-displayed-on
+    # http://stackoverflow.com/questions/17615414/how-to-convert-binary-string-to-normal-string-in-python3
     # http://stackoverflow.com/questions/13744473/command-line-execution-in-different-folder
     cwd = kwargs.pop('cwd', None)
     process = Popen(
@@ -35,18 +36,19 @@ def cmdline(command, *args, **kwargs):
 def evaluator_worker(evaluator_function, submission_status_set):
     if not evaluator_function:
         for s in submission_status_set:
-            s.status="ERROR_EVALUATOR"
+            s.status = "ERROR_EVALUATOR"
             s.save()
         return
     else:
         try:
             for s in submission_status_set:
-                submission = s.submission #ugly; works because submission should be the same for all
+                # ugly; works because submission should be the same for all
+                submission = s.submission
                 s.status = "PROCESSING"
                 s.save()
             result_dictionary = evaluator_function(
-                privatedata = submission.subtrack.private_data_unpacked_folder(), 
-                resultdata = submission.resultfile.name,
+                privatedata=submission.subtrack.private_data_unpacked_folder(),
+                resultdata=submission.resultfile.name,
                 )
             for s in submission_status_set:
                 benchname = s.benchmark.name
@@ -60,9 +62,10 @@ def evaluator_worker(evaluator_function, submission_status_set):
                     s.save()
         except:
             for s in submission_status_set:
-                s.status="ERROR_PROCESSING"
+                s.status = "ERROR_PROCESSING"
                 s.save()
             return
+
 
 def random_numbers(*args, **kwargs):
     sleep(20)
@@ -72,10 +75,17 @@ def random_numbers(*args, **kwargs):
     }
     return result
 
+
 def icfhr14_kws_tool(*args, **kwargs):
-    executable_folder = '{}/competitions/executables/VCGEvalConsole.linux'.format(settings.BASE_DIR)    
-    resultdata = kwargs.pop('resultdata', '{}/WordSpottingResultsSample.xml'.format(executable_folder))
-    privatedata = kwargs.pop('privatedata', '{}/GroundTruthRelevanceJudgementsSample.xml'.format(executable_folder))
+    executable_folder = \
+        '{}/competitions/executables/VCGEvalConsole.linux'\
+        .format(settings.BASE_DIR)
+    resultdata = kwargs.pop('resultdata',
+                            '{}/WordSpottingResultsSample.xml'
+                            .format(executable_folder))
+    privatedata = kwargs.pop('privatedata',
+                             '{}/GroundTruthRelevanceJudgementsSample.xml'.
+                             format(executable_folder))
     n_xml = 0
     if isdir(privatedata):
         for fn in listdir(privatedata):
@@ -87,14 +97,18 @@ def icfhr14_kws_tool(*args, **kwargs):
         n_xml = 1
 
     if(n_xml != 1):
-        raise IOError('The private data folder does not contain exactly one ground-truth file')
+        raise IOError('The private data folder does not contain exactly ' +
+                      'one ground-truth file')
 
     executable = '{}/VCGEvalConsole.sh'.format(executable_folder)
     commandline = '{} {} {}'.format(executable, privatedata, resultdata)
     command_output = cmdline(commandline)
 
-    rgx = r'ALL QUERIES\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'
-    r = re.search(rgx, command_output) 
+    rgx = r'ALL QUERIES\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'\
+        '\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'\
+        '\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'\
+        '\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'
+    r = re.search(rgx, command_output)
     result = {
         'p@5':              r.group(1),
         'p@10':             r.group(2),
@@ -102,23 +116,29 @@ def icfhr14_kws_tool(*args, **kwargs):
         'map':              r.group(4),
         'ndcg-binary':      r.group(5),
         'ndcg':             r.group(6),
-        'pr-curve':         dumps([r.group(7), r.group(8), r.group(9), r.group(10), r.group(11), r.group(12), r.group(13), r.group(14), r.group(15), r.group(16), r.group(17)])
+        'pr-curve':         dumps([r.group(7), r.group(8), r.group(9),
+                                   r.group(10), r.group(11), r.group(12),
+                                   r.group(13), r.group(14), r.group(15),
+                                   r.group(16), r.group(17)])
     }
     return result
 
+
 def transkribusBaseLineMetricTool(*args, **kwargs):
-    executable_folder = '{}/competitions/executables/TranskribusBaseLineMetricTool'.format(settings.BASE_DIR)    
-    #resultdata = kwargs.pop('resultdata', 'reco.lst')
+    executable_folder =\
+        '{}/competitions/executables/TranskribusBaseLineMetricTool'\
+        .format(settings.BASE_DIR)
+    # resultdata = kwargs.pop('resultdata', 'reco.lst')
     resultdata = kwargs.pop('resultdata', executable_folder)
-    #privatedata = kwargs.pop('privatedata', 'truth.lst')
+    # privatedata = kwargs.pop('privatedata', 'truth.lst')
     privatedata = kwargs.pop('privatedata', executable_folder)
 
     executable_jar = 'baselineTool.jar'
     if(isdir(privatedata)):
         print(resultdata)
         print(privatedata)
-        #This is the non-test scenario
-        #Hence we have to create a temporary folder and copy everything there
+        # This is the non-test scenario
+        # Hence we have to create a temporary folder and copy everything there
         newfolder = '{}{}/'.format(temporary_folder, uuid4().hex)
         makedirs(newfolder)
         if(isdir(resultdata)):
@@ -127,7 +147,7 @@ def transkribusBaseLineMetricTool(*args, **kwargs):
                 target_filename = join(newfolder, filename)
                 copyfile(full_filename, target_filename)
         else:
-            #If it is a file, it must be a tarball, or else raise an error 
+            # If it is a file, it must be a tarball, or else raise an error
             tar = tarfile.open(resultdata)
             tar.extractall(newfolder)
             tar.close()
@@ -135,19 +155,21 @@ def transkribusBaseLineMetricTool(*args, **kwargs):
             full_filename = join(privatedata, filename)
             target_filename = join(newfolder, filename)
             copyfile(full_filename, target_filename)
-        copyfile(join(executable_folder, executable_jar), join(newfolder, executable_jar))
+        copyfile(join(executable_folder, executable_jar), join(newfolder,
+                                                               executable_jar))
         executable_folder = newfolder
         resultdata = '{}reco.lst'.format(newfolder)
         privatedata = '{}truth.lst'.format(newfolder)
-   
+
     executable = 'java -jar {}'.format(executable_jar)
     commandline = '{} {} {}'.format(executable, privatedata, resultdata)
     command_output = cmdline(commandline, cwd=executable_folder)
 
     rmtree(newfolder)
     print(command_output)
-    rgx = r'Avg \(over Pages\) Avg Precision: ([\d\.]+)\nAvg \(over Pages\) Avg Recall: ([\d\.]+)\nAvg \(over Pages\) Avg F-Measure: ([\d\.]+)'
-    r = re.search(rgx, command_output)     
+    rgx = r'Avg \(over Pages\) Avg Precision: ([\d\.]+)\nAvg \(over Pages\) '\
+        'Avg Recall: ([\d\.]+)\nAvg \(over Pages\) Avg F-Measure: ([\d\.]+)'
+    r = re.search(rgx, command_output)
     result = {
         'bl-avg-precision': r.group(1),
         'bl-avg-recall':    r.group(2),
@@ -155,26 +177,32 @@ def transkribusBaseLineMetricTool(*args, **kwargs):
     }
     return result
 
+
 def transkribusErrorRate(*args, **kwargs):
-    #the method assumes the following parameters:
+    # the method assumes the following parameters:
     # kwargs has to contain "privatedata" with the path to a tar-file.
     # The tar-file has to contain Page-xml-files without subfolders.
-    # The TextEquiv of the lines have to be the ground truth for the competition.
+    # The TextEquiv of the lines have to be the ground truth for the
+    # competition.
     # kwargs also has to contain "resultdata",
-    # which is the path to the tar-file containing the hypothesises of the competitor.
-    # kwargs can contain a path "tmpfolder" - all temporary files and folder are created there.
+    # which is the path to the tar-file containing the hypothesises of the
+    # competitor.
+    # kwargs can contain a path "tmpfolder" - all temporary files and folder
+    # are created there.
     # the folder will be deleted afterwards, when it did not exist before.
     # Otherwise only the containing files and folders are deleted.
 
-    folder_data =  kwargs.pop('tmpfolder',normpath(abspath(".")))
-    folder_exec = kwargs.pop("execpath", join(normpath(abspath(".")),"executables/TranskribusErrorRate"))
-    privatedata =  kwargs.pop('privatedata',"gt.tgz")
-    resultdata =  kwargs.pop('resultdata', "hyp.tgz")
+    folder_data = kwargs.pop('tmpfolder', normpath(abspath(".")))
+    folder_exec = kwargs.pop("execpath",
+                             join(normpath(abspath(".")),
+                                  "executables/TranskribusErrorRate"))
+    privatedata = kwargs.pop('privatedata', "gt.tgz")
+    resultdata = kwargs.pop('resultdata', "hyp.tgz")
     print("privatedata is '"+privatedata+"'.")
-    print("resultdata is '"+resultdata+"'.")
-    print("folder_exec is '"+folder_exec+"'.")
-    params=kwargs.pop("params","")
-    file_exec = join(folder_exec,'TranskribusErrorRate.jar')
+    print("resultdata is '" + resultdata+"'.")
+    print("folder_exec is '" + folder_exec+"'.")
+    params = kwargs.pop("params", "")
+    file_exec = join(folder_exec, 'TranskribusErrorRate.jar')
 
     data_lists = {}
     to_remove_folder = []
@@ -183,13 +211,13 @@ def transkribusErrorRate(*args, **kwargs):
     if not (isdir(folder_data)):
         print("folder have to be deleted")
         deleteroot = True
-    for (file_tar,prefix) in [[privatedata,"gt"], [resultdata,"hyp"]]:
-        print("execute '"+ file_tar + "' ...")
-        folder_xmls = join(folder_data,prefix + '_dir')
+    for (file_tar, prefix) in [[privatedata, "gt"], [resultdata, "hyp"]]:
+        print("execute '" + file_tar + "' ...")
+        folder_xmls = join(folder_data, prefix + '_dir')
         print(folder_xmls)
         if isdir(folder_xmls):
             rmtree(folder_xmls)
-        makedirs(folder_xmls) #, exist_ok=True)
+        makedirs(folder_xmls)  # , exist_ok=True)
         to_remove_folder += [folder_xmls]
         print("unpack tar-file ...")
         obj_tar = tarfile.open(file_tar)
@@ -198,21 +226,23 @@ def transkribusErrorRate(*args, **kwargs):
         print("unpack tar-file ... [DONE]")
         print("save list ...")
         files_xml = sorted(listdir(folder_xmls))
-        file_list_xml = join(folder_data,prefix + '.lst')
-        to_remove_file+=[file_list_xml]
+        file_list_xml = join(folder_data, prefix + '.lst')
+        to_remove_file += [file_list_xml]
         data_lists[file_tar] = file_list_xml
         with open(file_list_xml, 'w') as tfFile:
             for file_xml in files_xml:
                 tfFile.write(join(folder_xmls, file_xml)+"\n")
-                #print(join(folder_xmls, file_xml), file=tfFile)
+#                print(join(folder_xmls, file_xml), file=tfFile)
         print("save list ... [DONE] (to '"+file_list_xml+"')")
     if (deleteroot):
         to_remove_folder += [folder_data]
 
-    executable = 'java -cp {} eu.transkribus.errorrate.ErrorRateParser'.format(file_exec)
-    commandline = '{} {} {} {}'.format(executable,params, data_lists[privatedata], data_lists[resultdata])
+    executable = 'java -cp {} eu.transkribus.errorrate.ErrorRateParser'.format(
+        file_exec)
+    commandline = '{} {} {} {}'.format(
+        executable, params, data_lists[privatedata], data_lists[resultdata])
     print(commandline)
-    #command_output = "tryrun"
+#    command_output = "tryrun"
     command_output = cmdline(commandline)
     print("output of algorithm:")
     print(command_output)
@@ -223,25 +253,28 @@ def transkribusErrorRate(*args, **kwargs):
     for folder in to_remove_folder:
         print("remove '"+folder+"'")
         rmtree(folder)
-    rgx = r'.*SUB = ([\d\.]+).*\nDEL = ([\d\.]+).*\nINS = ([\d\.]+).*\nERR = ([\d\.]+).*'
+    rgx = r'.*SUB = ([\d\.]+).*\nDEL = ([\d\.]+).*\nINS '\
+        '= ([\d\.]+).*\nERR = ([\d\.]+).*'
     r = re.search(rgx, command_output)
     result = {
-    'ERR': r.group(4).encode("utf-8"),
-    'INS': r.group(3).encode("utf-8"),
-    'DEL': r.group(2).encode("utf-8"),
-    'SUB': r.group(1).encode("utf-8"),
+        'ERR': r.group(4).encode("utf-8"),
+        'INS': r.group(3).encode("utf-8"),
+        'DEL': r.group(2).encode("utf-8"),
+        'SUB': r.group(1).encode("utf-8"),
     }
     return result
 
+
 def icfhr16_HTR_tool(*args, **kwargs):
     print("icfhr16_HTR_tool")
-    executable_folder = '{}/competitions/executables/EvaluationCERandWER'.format(settings.BASE_DIR)
+    executable_folder = '{}/competitions/executables/'\
+        'EvaluationCERandWER'.format(settings.BASE_DIR)
     resultdata = kwargs.pop('resultdata', executable_folder)
-    privatedata = kwargs.pop('privatedata', '{}/gt.zip'.format(executable_folder))
+    privatedata = kwargs.pop('privatedata',
+                             '{}/gt.zip'.format(executable_folder))
 
     print(resultdata)
     print(privatedata)
-
 
     executable = '{}/Create_WER-PAGE.sh'.format(executable_folder)
     commandline = '{} {} {}'.format(executable, resultdata, privatedata)
@@ -254,6 +287,36 @@ def icfhr16_HTR_tool(*args, **kwargs):
     result = {
         'CER':             r.group(1),
         'WER':             r.group(2),
+    }
+    print(result)
+    return result
+
+
+def icdar2017_writer_identification(*args, **kwargs):
+    print("ICDAR 2017 Writer Identification")
+    print(str(kwargs))
+    executable_folder = \
+        '{}/competitions/executables/ICDAR2017WriterIdentification'\
+        .format(settings.BASE_DIR)
+    resultdata = kwargs.pop('resultdata', executable_folder)
+    privatedata = kwargs.pop('privatedata',
+                             '{}/gtfile.txt'.format(executable_folder))
+
+    print(resultdata)
+    print(privatedata)
+
+    executable = '{}/evaluation.py'.format(executable_folder)
+    commandline = '{} {} {}'.format(executable, privatedata +
+                                    '/gtfile.csv', resultdata)
+    print(commandline)
+
+    command_output = cmdline(commandline)
+
+    rgx = r'([\d\.]+)\n+([\d\.]+)'
+    r = re.search(rgx, command_output)
+    result = {
+        'precision': r.group(1),
+        'map': r.group(2),
     }
     print(result)
     return result
---
+++ scriptnet/competitions/executables/ICDAR2017WriterIdentification/evaluation.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+"""Evaluation Protocol for the ICDAR 2017 Writer Identification Competition on
+Historical Data
+
+This script evaluates the results submitted to ScriptNet
+
+"""
+import logging
+
+__author__ = "Stefan Fiel"
+__copyright__ = "Copyright 2017, Computer Vision Lab, TU Wien"
+__credits__ = "Stefan Fiel"
+__license__ = "GPLv3"
+__version__ = "0.1"
+__maintainer__ = "Stefan Fiel"
+__email__ = "fiel@caa.tuwien.ac.at"
+__status__ = "Production"
+
+OUTPUT_NAME = "Historical-WI Evaluation"
+
+logging.basicConfig(level=logging.INFO, format=OUTPUT_NAME +
+                    ' %(levelname)s %(message)s')
+
+logger = logging.getLogger()
+
+
+def parseGTFile(gtfile):
+    import re
+
+    file = open(gtfile, "r")
+
+    gt = {}
+    writer = {}
+    for line in file:
+        line = line.rstrip('\n')
+        s = line.split('=')
+        if len(s) != 2:
+            logger.error('syntax of GT file incorrect: ' + line)
+            exit(-2)
+        gt[s[0]] = s[1]
+        w = re.split('_|-', s[1])
+        if len(w) == 0:
+            logger.error('syntax of GT file incorrect: ' + line)
+            exit(-3)
+
+        writer[s[0]] = w[0]
+    file.close()
+    return gt, writer
+
+
+def evaluate(writer, resultfile):
+    file = open(resultfile, "r")
+
+    page_count = 0
+    top1_count = 0
+
+    average_precision = []
+    for line in file:
+
+        line = line.rstrip()
+        imgs = line.split(',')
+
+        ref_page = imgs.pop(0)
+        ref_writer = writer[ref_page]
+        logger.debug("ref_writer:" + ref_writer + " ref_page:" + ref_page)
+        if ref_writer == writer[imgs[0]]:
+            top1_count = top1_count + 1
+        i_count = 0
+        i_true = 0
+        cur_sum = 0
+        for i in imgs:
+            i_count = i_count + 1
+            if writer[i] == ref_writer:
+                i_true = i_true + 1
+                cur_sum = cur_sum + (float)(i_true)/i_count
+
+        page_count = page_count + 1
+        average_precision.append((float)(cur_sum)/i_true)
+
+    file.close()
+    logger.debug("top1_count:" + str(top1_count))
+    logger.debug("page_count:" + str(page_count))
+    precision = (float)(top1_count)/page_count
+    print("len avg:" + str(average_precision))
+    meanap = float(sum(average_precision))/float(len(average_precision))
+    return precision, meanap
+
+if __name__ == "__main__":
+    import argparse
+    import os.path
+
+    logger.setLevel(logging.CRITICAL)
+    logger.debug("starting")
+    parser = argparse.ArgumentParser(
+        description='evaluation the result files submitted via ScriptNet ' +
+        'of the ICDAR 2017 Historic-WI')
+
+    parser.add_argument('gtfile', metavar='[GT file]', help='' +
+                        'GroundTruth file of the competition with mapping' +
+                        '[obfuscated filename]=[real filename]. Real ' +
+                        'Filename must look like [writerId]_....')
+    parser.add_argument('resultfile', metavar='[result file]', help='' +
+                        'Result file submitted for 2017 Historical-WI')
+
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.gtfile):
+        logger.error("GT file does not exist!")
+        exit(-1)
+    else:
+        gt, writer = parseGTFile(args.gtfile)
+
+    if not os.path.isfile(args.resultfile):
+        logger.error("result file does not exist!")
+        exit(-1)
+    else:
+        precision, meanap = evaluate(writer, args.resultfile)
+        logger.info("precision:" + str(precision))
+        logger.info("map:" + str(meanap))
+        print(precision)
+        print(meanap)
