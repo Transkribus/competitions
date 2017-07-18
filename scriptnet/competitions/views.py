d2652e42d95188efcb519a92e96a13caeb167f47
Giorgos Sfikas <georges.sfikas@gmail.com>
2017-07-18 18:51:17 +0300

    Revert

--- scriptnet/competitions/views.py
+++ scriptnet/competitions/views.py
@@ -466,7 +466,7 @@ def viewresults(request, competition_id, track_id, subtrack_id):
         if not s.publishable:
             if not request.user.is_authenticated():
                 continue
-            if request.user.individual in competition.organizer_set.all() or request.user.is_superuser: 
+            if request.user.is_superuser: 
                 pass
             elif not request.user.individual in s.submitter.all():
                 continue
